1cd73054cd271028c11c03f86012b84e37ecf824
MD RASHID <111352218+rashidgh@users.noreply.github.com>
2022-11-14 13:55:59 +0530

    Add files via upload
    urban clone with tailwind css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './ComponentsComposition/Header/Navbar';
+import Main from './ComponentsComposition/Main/Main';
+import Footer from './ComponentsComposition/Footer/Footer';
+
+const App = () => {
+  return (
+    <div id="navBlock"><Navbar/><Main/><Footer/></div>
+  )
+}
+
+
+export default App
+
+
---
+++ src/ComponentsComposition/Header/Menu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="menuBlock basis-4/5 relative">
+      <ul className="absolute right=[2%] flex list-none left-2/4 ml-[12%] mt-[-1%] ">
+        <li className="px-4">
+          <a className="inline-block  pb-2.5 font-bold mt-[-20px]">Blog</a>
+        </li>
+        <li className="px-4 font-bold">
+          <a className="inline-block  pb-2.5 no-underline">
+            <u>Register As A Professional</u>
+          </a>
+        </li>
+        <li className="px-4 font-bold">
+          <a className="inline-block  pb-2.5 no-underline">Login/</a>
+          <a className="inline-block  pb-2.5 no-underline">Sign Up</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
